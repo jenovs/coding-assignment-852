a72e13d93f65ea08a0f1401f58465ba2b5bce350
Viktors Jenovs <jviktors@gmail.com>
2018-05-12 11:03:48 +0300

    Disable linter warnings

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,8 +1,10 @@
 import response from '../response.json';
 
+// eslint-disable-next-line
 const fetchData = () =>
   fetch('https://api.myjson.com/bins/oivjj').then(res => res.json());
 
+// eslint-disable-next-line
 const fakeFetch = () => new Promise(resolve => resolve(response));
 
 export default fetchData;
