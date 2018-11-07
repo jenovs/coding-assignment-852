d6999ed16226818344f396f77d84e3772911a840
Viktors Jenovs <jviktors@gmail.com>
2018-11-07 15:45:48 +0200

    Updates

---
+++ precache-manifest.bbaff84f55dc59094b235aeb07208d17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "418f39c9aa62a922c8f29c81c81515f7",
+    "url": "/react-serp/static/media/star.418f39c9.svg"
+  },
+  {
+    "revision": "b28a80becd315079d4306add7623fbb3",
+    "url": "/react-serp/static/media/heart.b28a80be.svg"
+  },
+  {
+    "revision": "bc9b5f255d830d9a1ccd",
+    "url": "/react-serp/static/js/runtime~main.bc9b5f25.js"
+  },
+  {
+    "revision": "bff76ceac21044590a29",
+    "url": "/react-serp/static/js/main.bff76cea.chunk.js"
+  },
+  {
+    "revision": "8a5f99092895197e3f76",
+    "url": "/react-serp/static/js/1.8a5f9909.chunk.js"
+  },
+  {
+    "revision": "bff76ceac21044590a29",
+    "url": "/react-serp/static/css/main.03b356dc.chunk.css"
+  },
+  {
+    "revision": "b806c84c32c47b745d839c61a8a5c3d2",
+    "url": "/react-serp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/coding-assignment-852/precache-manifest.989eb85ff073ba8558ce9e3331d7b7ea.js"
+  "/react-serp/precache-manifest.bbaff84f55dc59094b235aeb07208d17.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/coding-assignment-852/index.html", {
+workbox.routing.registerNavigationRoute("/react-serp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
